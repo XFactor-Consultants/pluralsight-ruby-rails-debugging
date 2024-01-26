c930cad0e04c9b6e73528ab7feab268e97328815
Henchworm <agop5134@gmail.com>
2024-01-25 21:26:56 -0700

    initial starter repo for Pluralsigth debugging

---
+++ app/models/wiki_post.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class WikiPost < ApplicationRecord
+  has_one_attached :image
+  has_one :wiki_post_record, dependent: :destroy
+  after_save :create_wiki_post_record
+  after_find :create_wiki_post_record
+
+  scope :contributors, -> { pluck(:author).uniq }
+
+  private
+
+  def create_wiki_post_record
+    data = {
+      id: self.id,
+      created_at: self.created_at,
+      updated_at: self.updated_at,
+      title: self.title,
+      description: self.description,
+      author: self.author
+    }
+    build_wiki_post_record(data: data).save!
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  resources :wiki_posts
+  namespace :wiki_posts do
+    get 'example'
+  end
+  namespace :welcome do
+    get 'index'
+    get 'about'
+    get 'archive'
+  end
+
+  get '/about', to: redirect('/welcome/about')
+  root 'welcome#index'
+end
